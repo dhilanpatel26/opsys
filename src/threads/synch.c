0d5bf61e18925d6082389a72229b0a0966331be0
dhilanpatel26 <dpatel93@jhu.edu>
2025-02-16 19:35:28 -0500

    [2.1] added semaphore priority scheduling, [3.1] restored mlfqs block yield and generalized

--- src/threads/synch.c
+++ src/threads/synch.c
@@ -69,8 +69,8 @@ sema_down (struct semaphore *sema)
   old_level = intr_disable ();
   while (sema->value == 0) 
     {
+      // TODO: add support for dynamic resorting as priorities change, mlfq and prischeder
       list_insert_ordered(&sema->waiters, &thread_current ()->elem,thread_compare_priority, NULL);
-      // list_push_back (&sema->waiters, &thread_current ()->elem);
       thread_block ();
     }
   sema->value--;
@@ -114,11 +114,24 @@ sema_up (struct semaphore *sema)
 
   ASSERT (sema != NULL);
 
-  old_level = intr_disable ();
-  if (!list_empty (&sema->waiters)) 
-    thread_unblock (list_entry (list_pop_front (&sema->waiters),
-                                struct thread, elem));
+  // doesn't need to be part of crit section
+  // this is only used doe correctness checking
   sema->value++;
+
+  // critical section: must synchronize sema->waiters list
+  old_level = intr_disable ();
+  if (!list_empty (&sema->waiters)) {
+    struct thread *t = list_entry (list_pop_front (&sema->waiters), struct thread, elem);
+    thread_unblock (t);
+
+    if (!intr_context()) {
+      struct thread *cur = thread_current();
+      if (t->priority > cur->priority) {
+        thread_yield();
+      }
+    }
+  }
+
   intr_set_level (old_level);
 }
 
@@ -303,7 +316,8 @@ cond_wait (struct condition *cond, struct lock *lock)
   ASSERT (lock_held_by_current_thread (lock));
   
   sema_init (&waiter.semaphore, 0);
-  list_push_back (&cond->waiters, &waiter.elem);
+  // list_push_back (&cond->waiters, &waiter.elem);
+  list_insert_ordered(&cond->waiters, &waiter.elem, thread_compare_priority, NULL);
   lock_release (lock);
   sema_down (&waiter.semaphore);
   lock_acquire (lock);
