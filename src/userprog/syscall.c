b7864d7ac72d6bad873ee9ed2507fbf08166a76b
dhilanpatel26 <dpatel93@jhu.edu>
2025-03-05 12:48:47 -0500

    Scaffolding for handling system calls

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -1,8 +1,14 @@
+#ifndef USERPROG
+#define USERPROG
+#endif
+
 #include "userprog/syscall.h"
 #include <stdio.h>
 #include <syscall-nr.h>
 #include "threads/interrupt.h"
 #include "threads/thread.h"
+#include "threads/vaddr.h"
+#include "userprog/pagedir.h"
 
 static void syscall_handler (struct intr_frame *);
 
@@ -16,5 +22,32 @@ static void
 syscall_handler (struct intr_frame *f UNUSED) 
 {
   printf ("system call!\n");
+  
+  // relevant stack data is 4 bytes and aligned
+  int *esp = f->esp; // user virtual memory
+
+  // TODO: validate esp
+  if (!is_user_vaddr(esp)) {
+    thread_exit();
+  }
+
+  int *addr = pagedir_get_page(thread_current()->pagedir, esp);
+
+  if (addr == NULL) {
+    thread_exit();
+  }
+
+  int syscall_number = *addr;
+
+  switch (syscall_number) {
+    case SYS_EXIT:
+      int status = *(addr + 1);
+      break;
+    default:
+      NOT_REACHED();
+  }
+
+
+
   thread_exit ();
 }
