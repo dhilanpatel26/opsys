d21e6699640b348d8b20d0b25b0105d43e5b809b
dhilanpatel26 <dpatel93@jhu.edu>
2025-03-05 22:43:28 -0500

    Fixing compile-time errors

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -30,17 +30,17 @@ syscall_handler (struct intr_frame *f UNUSED)
   // relevant stack data is 4 bytes and aligned
   int *esp = f->esp; // user virtual memory
 
-  int syscall_number = *(int*) translate_vaddr(esp);
+  int syscall_number = *(int*) translate_uvaddr(esp);
 
   switch (syscall_number) {
     case SYS_WAIT: {
-      int pid = *(int*) translate_vaddr(esp + 1);
+      int pid = *(int*) translate_uvaddr(esp + 1);
       int status = process_wait((tid_t) pid); // 1:1 mapping of proc to thread
       f->eax = status; // return status to user program
       break;
     }
     case SYS_EXIT: {
-      int status = *(int*) translate_vaddr(esp + 1);
+      int status = *(int*) translate_uvaddr(esp + 1);
       exit_handler(status);
       NOT_REACHED();
     }
