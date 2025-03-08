6c5bbb80c9fe16b0c37949da780815d378cfa6a9
dhilanpatel26 <dpatel93@jhu.edu>
2025-03-08 00:47:15 -0500

    Create system call handler and general error checking

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -10,6 +10,7 @@
 #include "threads/vaddr.h"
 #include "userprog/pagedir.h"
 #include "userprog/process.h"
+#include "filesys/filesys.h"
 
 static void syscall_handler (struct intr_frame *);
 static void exit_handler (int status);
@@ -34,37 +35,86 @@ syscall_handler (struct intr_frame *f UNUSED)
 
   switch (syscall_number) {
     case SYS_WAIT: {
-      int pid = *(int*) translate_uvaddr(esp + 1);
+      int *pid_ptr = (int*) translate_uvaddr(esp + 1);
+      if (pid_ptr == NULL) {
+        f->eax = -1;
+        return;
+      }
+      int pid = *pid_ptr;
       int status = process_wait((tid_t) pid); // 1:1 mapping of proc to thread
       f->eax = status; // return status to user program
       break;
     }
     case SYS_EXIT: {
-      int status = *(int*) translate_uvaddr(esp + 1);
+      int *status_ptr = (int*) translate_uvaddr(esp + 1);
+      if (status_ptr == NULL) {
+        f->eax = -1;
+        return;
+      }
+      int status = *status_ptr;
       exit_handler(status);
       NOT_REACHED();
+    }
+    case SYS_CREATE: {
+      char **file_name_ptr = (char**) translate_uvaddr(esp + 1);
+      if (file_name_ptr == NULL) {
+        f->eax = -1;
+        return;
+      }
+      char *file_name = *file_name_ptr;
+      if (file_name == NULL) {
+        f->eax = -1;
+        return;
+      }
+      if (!validate_string(file_name)) {
+        f->eax = -1;
+        return;
+      }
+      unsigned *initial_size_ptr = (unsigned*) translate_uvaddr(esp + 2);
+      if (initial_size_ptr == NULL) {
+        f->eax = -1;
+        return;
+      }
+      unsigned initial_size = *initial_size_ptr;
+      bool success = filesys_create(file_name, initial_size);
+      f->eax = success;
+      break;
     }
     default:
       NOT_REACHED();
   }
 }
 
+static bool validate_string(const char *str) {
+  if (str == NULL) {
+    return false;
+  }
+  for (;; str++) {
+    if (translate_uvaddr((void*)str) == NULL) {
+      return false;
+    }
+    if (*str == '\0') {
+      break;
+    }
+  }
+  return true;
+}
+
 static void *
 translate_uvaddr(void *uptr) {
-  if (!is_user_vaddr(uptr)) {
-    thread_exit();
+  if (uptr == NULL || !is_user_vaddr(uptr)) {
+    return NULL;
   }
   
   void *kptr = pagedir_get_page(thread_current()->pagedir, uptr);
   
   if (kptr == NULL) {
-    thread_exit();
+    return NULL;
   }
 
   return kptr;
 }
 
-// TODO: maybe move this to process.c
 static void
 exit_handler (int status) {
   struct thread *cur = thread_current();
