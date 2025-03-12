ae6b12204d8f0e29f25c625a6a9adca1a306ad02
dhilanpatel26 <dpatel93@jhu.edu>
2025-03-12 10:20:49 -0400

    Attempted page fault handling

--- src/userprog/exception.c
+++ src/userprog/exception.c
@@ -4,6 +4,7 @@
 #include "userprog/gdt.h"
 #include "threads/interrupt.h"
 #include "threads/thread.h"
+#include "threads/vaddr.h"
 
 /* Number of page faults processed. */
 static long long page_fault_cnt;
@@ -148,14 +149,26 @@ page_fault (struct intr_frame *f)
   write = (f->error_code & PF_W) != 0;
   user = (f->error_code & PF_U) != 0;
 
-  /* To implement virtual memory, delete the rest of the function
-     body, and replace it with code that brings in the page to
-     which fault_addr refers. */
-  printf ("Page fault at %p: %s error %s page in %s context.\n",
-          fault_addr,
-          not_present ? "not present" : "rights violation",
-          write ? "writing" : "reading",
-          user ? "user" : "kernel");
-  kill (f);
+  if (user) {
+   intr_dump_frame (f);
+   thread_exit ();
+  } else if (fault_addr < PHYS_BASE && is_user_vaddr(fault_addr)) {
+   f->eip = (void*) f->eax;
+   f->eax = 0xffffffff;
+   return;
+  } else {
+   intr_dump_frame (f);
+   PANIC ("Kernel page fault");
+  }
+
+//   /* To implement virtual memory, delete the rest of the function
+//      body, and replace it with code that brings in the page to
+//      which fault_addr refers. */
+//   printf ("Page fault at %p: %s error %s page in %s context.\n",
+//           fault_addr,
+//           not_present ? "not present" : "rights violation",
+//           write ? "writing" : "reading",
+//           user ? "user" : "kernel");
+//   kill (f);
 }
 
